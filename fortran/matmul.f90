5d3276b31b41364db548804b7ef434e04374f33e
Aaron Bulmahn <aaronbulmahn@googlemail.com>
2016-11-03 15:01:33 +0100

    generate matrix in fortran

--- fortran/matmul.f90
+++ fortran/matmul.f90
@@ -1,64 +1,51 @@
-module matrix
-    contains
-    function matmul(matrix_a, matrix_b) result(matrix_r)
-        implicit none
-        double precision, intent(in), dimension(:, :) :: matrix_a, matrix_b
-        double precision, dimension(:, :), allocatable :: matrix_r
-        integer :: n, i, j, k
-        double precision :: s
+function matmul(matrix_a, matrix_b) result(matrix_r)
+    implicit none
+    double precision, intent(in), dimension(:, :) :: matrix_a, matrix_b
+    double precision, dimension(:, :), allocatable :: matrix_r
+    integer :: n, i, j, k
+    double precision :: s
 
-        n = size(matrix_a, 1)
-        allocate(matrix_r(n, n))
+    n = size(matrix_a, 1)
+    allocate(matrix_r(n, n))
 
-        !$OMP parallel shared(matrix_a, matrix_b, matrix_r, n) private(i, j, s, k)
+    !$OMP parallel shared(matrix_a, matrix_b, matrix_r, n) private(i, j, s, k)
 
-        !$OMP parallel do
-        do i = 1, n
-            do j = 1, n
-                s = 0.0
-                do k = 1, n
-                    s = s + matrix_a(i, k) * matrix_b(k, j)
-                end do
-                matrix_r(i, j) = s
+    !$OMP parallel do
+    do i = 1, n
+        do j = 1, n
+            s = 0.0
+            do k = 1, n
+                s = s + matrix_a(i, k) * matrix_b(k, j)
             end do
+            matrix_r(i, j) = s
         end do
-        !$OMP end parallel do
+    end do
+    !$OMP end parallel do
 
-        !$OMP end parallel
-    end function matmul
-
-    subroutine load_matrix(infile, matrix_a, matrix_b)
-        implicit none
-        character*(*) :: infile
-        double precision, intent(out), dimension(:, :), allocatable :: matrix_a, matrix_b
-        integer :: n, res
-
-        open(10, file=infile, access="sequential", form="formatted", iostat=res)
-
-        read(10, *) n
-
-        allocate(matrix_a(n, n))
-        allocate(matrix_b(n, n))
-
-        read(10, *) matrix_a
-        read(10, *) matrix_b
-
-        close(10)
-    end subroutine load_matrix
-end module
+    !$OMP end parallel
+end function matmul
 
 program main
-    use matrix
     implicit none
-    integer :: i, j
+    integer :: i, j, n
     real :: cl1, cl2
     double precision, dimension(:,:), allocatable :: matrix_a, matrix_b, matrix_r
-    character*(*), parameter :: infile = "matrix.csv"
+    character(len=32) :: arg
 
-    ! matrix_a = reshape((/ 0.2, 0.4, 0.6, 0.8, 0.1, 0.3, 0.5, 0.7, 0.9 /), shape(matrix_a))
-    ! matrix_b = reshape((/ 0.2, 0.4, 0.6, 0.8, 0.1, 0.3, 0.5, 0.7, 0.9 /), shape(matrix_b))
+    call get_command_argument(1, arg)
 
-    call load_matrix(infile, matrix_a, matrix_b)
+    if(len_trim(arg) == 0) then
+        write(0, *) "use: matmul <n>"
+        stop 9
+    end if
+
+    read(arg, '(i32)') n
+
+    allocate(matrix_a(n, n))
+    allocate(matrix_b(n, n))
+
+    call random_number(matrix_a)
+    call random_number(matrix_b)
 
     call cpu_time(cl1)
     matrix_r = matmul(matrix_a, matrix_b)
