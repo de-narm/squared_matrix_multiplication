a8d4489138dcc6cf2fac47a490d59c7230366a56
De Narm <s3152806@msx.tu-dresden.de>
2016-11-03 06:09:47 +0100

    fixed haskell version; improved performance

--- haskell/matrix_multiplication.hs
+++ haskell/matrix_multiplication.hs
@@ -1,41 +1,37 @@
 import System.Random
+import System.IO
 import System.Environment
 import Data.Time.Clock
+import Control.DeepSeq
+import Control.Parallel.Strategies
 
 main = do
-	getN
-	
+    getN
+
 getN = do
-	input <- getArgs;
+    input <- getArgs;
     --create random generators
-	g1 <- newStdGen;
-	g2 <- newStdGen;
+    g1 <- newStdGen;
+    g2 <- newStdGen;
         if input == []
             then do
-		        putStr "Please use `./matrix_multiplication <<int>>`\n";
+            putStr "Please use `./matrix_multiplication <<int>>`\n";
             else do 
                 --loop getN until we get an int
-	            case reads (head input) :: [(Int, String)] of 
-		            [(n, _)] -> randomFillMatrices n g1 g2 
-		            _ -> putStr "Please use `./matrix_multiplication <<int>>`\n"
+            case reads (head input) :: [(Int, String)] of 
+                [(n, _)] -> randomFillMatrices n g1 g2 
+                _ -> putStr "Please use `./matrix_multiplication <<int>>`\n"
 
 randomFillMatrices :: Int -> StdGen -> StdGen ->  IO() 
 randomFillMatrices i g1 g2 =
-	if  i > 0 
+    if  i > 0 
         --set up both lists with endless random values
-	    then initMul i (randomRs (0,1) g1) (randomRs (0,1) g2)
-	    else putStr "Only use positive numbers!"
+    then initMul i (randomRs (0,1) g1) (randomRs (0,1) g2)
+    else putStr "Only use positive numbers!"
 
 initMul :: Int -> [Double] -> [Double] -> IO()
 initMul i xs ys = do
-    putStr "\n";
-    --force haskell to set all needed random values before mul
-    print (xs!!((i*2)-1));
-    print (ys!!((i*2)-1));
-    putStr "\n";
-    --timestamp begin mul
-	print =<< getCurrentTime;
-	endMul i (doMul i 0 (getRows i xs) (getColumns i 0 ys));
+    endMul i (doMul i 0 (getRows i xs) (getColumns i 0 ys));
 
 getRows :: Int -> [Double] -> [[Double]]
 getRows i [] = []
@@ -55,32 +51,18 @@ singleColumn i j k xs =
 
 doMul :: Int -> Int -> [[Double]] -> [[Double]] -> [Double]
 doMul i j xs ys 
-    | j >= 0 && j < (i*i) = [scalar i 0 (xs!!(div j i)) (ys!!(mod j i))]
+    | j < (i*i) = [sum(zipWith (*) 
+        (head(drop ((div j i)-1) xs)) 
+        (head(drop ((mod j i)-1) xs)))]
         ++ doMul i (j+1) xs ys
     | otherwise = []
 
-scalar :: Int -> Int -> [Double] -> [Double] -> Double
-scalar i j xs ys 
-    | i > j = (xs!!j)*(ys!!j) + scalar i (j+1) xs ys
-    | otherwise = 0
-
 endMul :: Int -> [Double] -> IO()
-endMul i xs = do
-    putStr "\n";
-    --haskell is lazy, meaning it only does what it has to
-    --therefore we print the very last value of our new matrix
-    print (xs!!((i*i)-1));
-    putStr "\n";
+endMul i zs = do
+    --timestamp begin mul
+    tstpBeg <- getCurrentTime;
     --timestamp end mul
-    print =<< getCurrentTime;
-
---debug fuction to control values
-printMatrix :: Int -> Int -> [Double] -> IO()
-printMatrix i j xs =
-    if j > 0 
-        then do
-            putStr "\n";
-            print (take i xs);
-            printMatrix i (j-1) (drop i xs);
-        else do
-            putStr "\n";
+    tstpEnd <- (deepseq 
+        (using zs (parListChunk (div (i*i) 4) rdeepseq)) 
+        getCurrentTime);
+    print (diffUTCTime tstpEnd tstpBeg);
